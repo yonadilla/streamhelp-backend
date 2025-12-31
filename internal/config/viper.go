841d35b419bba30e34f3eb57784a610b6bf1a657
mousetri <yonadi322@gmail.com>
2025-12-31 21:41:09 +0700

    fix : typo using setConfig

--- internal/config/viper.go
+++ internal/config/viper.go
@@ -10,12 +10,12 @@ func NewViper() *viper.Viper{
 	config := viper.New()
 
 	config.SetConfigName("config")
-	config.SetConfigFile("json")
+	config.SetConfigType("json")
 	config.AddConfigPath(".././")
 	config.AddConfigPath("./")
 	err := config.ReadInConfig()
 	if err != nil {
-		panic(fmt.Errorf("Fatal error config file : %w \n", err))
+		panic(fmt.Errorf("Fatal error config file : %s \n", err))
 	}
 
 	return config
