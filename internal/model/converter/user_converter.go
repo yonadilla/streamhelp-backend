ea82cb66248fb91ede77bdb883c62ad77fd89e2a
mousetri <yonadi322@gmail.com>
2025-12-31 21:36:34 +0700

    chore : add user_converter

---
+++ internal/model/converter/user_converter.go
@@ -0,0 +1,29 @@
+package converter
+
+import (
+	"streamhelper-backend/internal/entity"
+	"streamhelper-backend/internal/model"
+)
+
+func UserToResponse(user *entity.User) *model.UserResponse{
+	return &model.UserResponse{
+		ID: 		user.ID,
+		Name: 		user.Name,
+		CreatedAt: 	user.CreatedAt,
+		UpdatedAt: 	user.UpdatedAt,
+	}
+}
+func UserToTokenResponse(user *entity.User) *model.UserResponse {
+	return &model.UserResponse{
+		Token: user.Token,
+	}
+}
+
+func UserToEvent(user *entity.User) *model.UserEvent {
+	return &model.UserEvent{
+		ID:        user.ID,
+		Name:      user.Name,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+}
